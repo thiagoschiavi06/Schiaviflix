30c1dbc91fdfb9582e9061d3ad1dc9bd6cdddabd
Thiago Schiavi <thiagoschiavi7@gmail.com>
2024-08-01 21:05:48 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 let trilho = document.getElementById('trilho')
+let body = document.querySelector('body')
+let titulo = document.querySelector('titulo')
+let categoria = document.querySelector('dark')
+
 
 trilho.addEventListener('click', ()=>{
     trilho.classList.toggle('dark')
+    body.classList.toggle('dark')
+    titulo.ClassList.toggle('dark')
+    categoria.classList.toggle('dark')
 })
