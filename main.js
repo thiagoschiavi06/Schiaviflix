6b9664f14bfc9d275a27914521c46e6c572b35df
Thiago Schiavi <thiagoschiavi7@gmail.com>
2024-08-02 10:47:49 -0300

    main.js

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-let trilho = document.getElementById('trilho')
-let body = document.querySelector('body')
-let titulo = document.querySelector('titulo')
-let categoria = document.querySelector('dark')
+let trilho = document.getElementById('trilho');
+let body = document.querySelector('body');
+let titulo = document.getElementById('titulo');
+let categoria = document.getElementById('categoria');
 
-
-trilho.addEventListener('click', ()=>{
-    trilho.classList.toggle('dark')
-    body.classList.toggle('dark')
-    titulo.ClassList.toggle('dark')
-    categoria.classList.toggle('dark')
-})
+trilho.addEventListener('click', () => {
+    trilho.classList.toggle('dark');
+    body.classList.toggle('dark');
+    titulo.classList.toggle('dark');
+    categoria.classList.toggle('dark');
+});
